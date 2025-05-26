e2486ae264754279cbb6d752b189737c8853cf50
Zachery Pipes <zacherypipes@gmail.com>
2025-05-26 16:54:35 -0500

    Created the Settings page which currently only hosts the authenticate button

---
+++ Carbon/Settings.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Carbon;
+
+public partial class SettingsPage : Page {
+    public SettingsPage() {
+        InitializeComponent();
+    }
+    
+    public async void Authenticate(object sender, RoutedEventArgs e) {
+        try {
+            AuthenticationState.LaunchAuthFlow();
+            await AuthenticationState.Instance.ExecuteEbayAuth();
+        } catch (Exception ex) {
+            MessageBox.Show($"Authorization failed: {ex.Message}");
+        }
+    }
+}
