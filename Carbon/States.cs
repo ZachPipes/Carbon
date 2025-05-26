2313b4fb322ef7d9c288c578626347995f617390
Zachery Pipes <zacherypipes@gmail.com>
2025-05-26 16:43:57 -0500

    Switched out all vars for their proper variables.

--- Carbon/States.cs
+++ Carbon/States.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -13,50 +14,77 @@ public sealed class AppState {
     public static AppState Instance => _instance.Value;
 
     private const string AppFolderName = "Carbon";
+    private const string TokenFileName = "Token.json";
 
     private AppState() {
         FilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), AppFolderName);
 
-        // Reading stored json
-        using var doc = JsonDocument.Parse(File.ReadAllText(FilePath));
-        var root = doc.RootElement;
+        string tokenFilePath = Path.Combine(FilePath, TokenFileName);
 
-        AccessToken = root.GetProperty(nameof(AccessToken)).GetString() ?? "";
-        AccessTokenExpiration = root.GetProperty(nameof(AccessTokenExpiration)).GetInt32();
-        RefreshToken = root.GetProperty(nameof(RefreshToken)).GetString() ?? "";
-        RefreshTokenExpiration = root.GetProperty(nameof(RefreshTokenExpiration)).GetInt32();
+        if (File.Exists(tokenFilePath)) {
+            try {
+                using JsonDocument doc = JsonDocument.Parse(File.ReadAllText(tokenFilePath));
+                JsonElement root = doc.RootElement;
+
+                AccessToken = "";
+                AccessTokenExpiration = root.GetProperty(nameof(AccessTokenExpiration)).GetInt32();
+                RefreshToken = root.GetProperty(nameof(RefreshToken)).GetString() ?? "";
+                RefreshTokenExpiration = root.GetProperty(nameof(RefreshTokenExpiration)).GetInt32();
+            } catch (Exception ex) {
+                Console.WriteLine($"Failed to parse Token.json: {ex.Message}");
+                SetDefaultTokens();
+            }
+        }
+        else {
+            SetDefaultTokens();
+        }
+
         IsSandboxMode = true;
     }
 
-    public async Task CheckForTokenUpdate() {
-        var currentTime = Utils.GetCurrentTimeInSeconds();
-        
-        if (currentTime > AccessTokenExpiration) {
-            var api = Instance.IsSandboxMode ? ".sandbox" : "";
-            var endpoint = $"https://api{api}.ebay.com/identity/v1/oauth2/token";
-            using var client = new HttpClient();
+    private void SetDefaultTokens() {
+        AccessToken = "";
+        AccessTokenExpiration = 0;
+        RefreshToken = "";
+        RefreshTokenExpiration = 0;
+    }
 
-            var credentials = $"{AuthenticationState.Instance.ClientId}:{AuthenticationState.Instance.ClientSecret}";
-            var base64Credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
+    public async Task CheckForTokenUpdate() {
+        int currentTime = Utils.GetCurrentTimeInSeconds();
+
+        if (currentTime > AccessTokenExpiration || AccessToken == "") {
+
+            string api = Instance.IsSandboxMode ? ".sandbox" : "";
+
+            string endpoint = $"https://api{api}.ebay.com/identity/v1/oauth2/token";
+
+            using HttpClient client = new();
+
+            string credentials = $"{AuthenticationState.Instance.ClientId}:{AuthenticationState.Instance.ClientSecret}";
+
+            string base64Credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
 
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Credentials);
 
-            var content = new FormUrlEncodedContent([
-                new("grant_type", "refresh_token"),
-                new("refresh_token", Instance.RefreshToken),
-                new("scope", AuthenticationState.Instance.Scopes ?? "")
+            FormUrlEncodedContent content = new([
+                new KeyValuePair<string,string>("grant_type", "refresh_token"),
+                new KeyValuePair<string,string>("refresh_token", Instance.RefreshToken),
+                new KeyValuePair<string,string>("scope", AuthenticationState.Instance.Scopes ?? "")
             ]);
 
-            var response = await client.PostAsync(endpoint, content);
-            var responseContent = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await client.PostAsync(endpoint, content);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
 
             if (response.IsSuccessStatusCode) {
-                var jsonDoc = JsonDocument.Parse(responseContent);
+                JsonDocument jsonDoc = JsonDocument.Parse(responseContent);
+
                 if (jsonDoc.RootElement.TryGetProperty("access_token", out var tokenElement)) {
                     Instance.AccessToken = tokenElement.GetString() ?? "";
+                    return;
                 }
 
-                throw new Exception("Access token not found in response.");
+                throw new Exception($"Access token not found in response.");
             }
 
             throw new Exception($"Failed to refresh token: {response.StatusCode} - {responseContent}");
@@ -79,9 +107,9 @@ public class AuthenticationState {
     private const string TokenFileName = "Token.json";
 
     private AuthenticationState() {
-        var credentialsFilePath = Path.Combine(AppState.Instance.FilePath, CredentialsFileName);
-        using var doc = JsonDocument.Parse(File.ReadAllText(credentialsFilePath));
-        var root = doc.RootElement;
+        string credentialsFilePath = Path.Combine(AppState.Instance.FilePath, CredentialsFileName);
+        using JsonDocument doc = JsonDocument.Parse(File.ReadAllText(credentialsFilePath));
+        JsonElement root = doc.RootElement;
 
         ClientId = root.GetProperty(nameof(ClientId)).GetString();
         ClientSecret = root.GetProperty(nameof(ClientSecret)).GetString();
@@ -89,7 +117,7 @@ public class AuthenticationState {
         Scopes = root.GetProperty(nameof(Scopes)).GetString();
 
         string[] jsonDataTypes = ["ClientId", "ClientSecret", "RedirectUri", "Scopes"];
-        foreach (var jsonDataType in jsonDataTypes) {
+        foreach (string jsonDataType in jsonDataTypes) {
             if (string.IsNullOrEmpty(jsonDataType)) {
                 Utils.ShowError($"Invalid credentials: Missing {jsonDataType}.");
             }
@@ -100,8 +128,8 @@ public class AuthenticationState {
     public static void LaunchAuthFlow() {
         // Launch the default browser with the url
         try {
-            var api = AppState.Instance.IsSandboxMode ? ".sandbox" : "";
-            var url = $"https://auth{api}.ebay.com/oauth2/authorize?client_id={Instance.ClientId}&redirect_uri={Instance.RedirectUri}&response_type=code&scope={Instance.Scopes}";
+            string api = AppState.Instance.IsSandboxMode ? ".sandbox" : "";
+            string url = $"https://auth{api}.ebay.com/oauth2/authorize?client_id={Instance.ClientId}&redirect_uri={Instance.RedirectUri}&response_type=code&scope={Instance.Scopes}";
 
             Process.Start(new ProcessStartInfo {
                 FileName = url,
@@ -115,56 +143,56 @@ public class AuthenticationState {
     // Executes back-end authentication
     public async Task ExecuteEbayAuth() {
         // Receive the url
-        var redirectUrl = Microsoft.VisualBasic.Interaction.InputBox("Sign in to your eBay account.\nThen paste the URL here:", "Enter URL");
+        string redirectUrl = Microsoft.VisualBasic.Interaction.InputBox("Sign in to your eBay account.\nThen paste the URL here:", "Enter URL");
 
         // Parse for the code
-        var uri = new Uri(redirectUrl);
-        var queryParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
-        var code = queryParams["code"];
+        Uri uri = new Uri(redirectUrl);
+        NameValueCollection queryParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
+        string code = queryParams["code"];
 
         if (string.IsNullOrEmpty(code)) {
             Utils.ShowError($"Code not found in url.");
             return;
         }
 
-        var response = await CodeForTokenExchange(code);
+        string response = await CodeForTokenExchange(code);
         using JsonDocument doc = JsonDocument.Parse(response);
-        var root = doc.RootElement;
+        JsonElement root = doc.RootElement;
 
-        AppState.Instance.AccessToken = root.GetProperty("access_token").GetString() ?? "EMPTY ACCESS TOKEN";
+        AppState.Instance.AccessToken = root.GetProperty("access_token").GetString()   ?? "EMPTY ACCESS TOKEN";
         AppState.Instance.RefreshToken = root.GetProperty("refresh_token").GetString() ?? "EMPTY REFRESH TOKEN";
 
         // Converting times
-        var jsonData = new Dictionary<string, object> {
+        Dictionary<string,object> jsonData = new() {
             ["AccessTokenExpiration"] = Utils.GetCurrentTimeInSeconds() + root.GetProperty("expires_in").GetInt32(),
             ["RefreshToken"] = root.GetProperty("refresh_token").GetString() ?? "EMPTY REFRESH TOKEN",
             ["RefreshTokenExpiration"] = Utils.GetCurrentTimeInSeconds() + root.GetProperty("refresh_token_expires_in").GetInt32(),
         };
 
-        var options = new JsonSerializerOptions {
+        JsonSerializerOptions options = new() {
             WriteIndented = true
         };
-        var json = JsonSerializer.Serialize(jsonData, options);
-        var tokenFilePath = Path.Combine(AppState.Instance.FilePath, TokenFileName);
+        string json = JsonSerializer.Serialize(jsonData, options);
+        string tokenFilePath = Path.Combine(AppState.Instance.FilePath, TokenFileName);
         await File.WriteAllTextAsync(tokenFilePath, json);
 
         Microsoft.VisualBasic.Interaction.MsgBox("EBay Authorization Complete!");
     }
 
     private async Task<string> CodeForTokenExchange(string code) {
-        using var client = new HttpClient();
+        using HttpClient client = new();
 
-        var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{ClientId}:{ClientSecret}"));
+        string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{ClientId}:{ClientSecret}"));
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
-        var requestBody = new FormUrlEncodedContent([
-            new("grant_type", "authorization_code"),
-            new("code", code),
-            new("redirect_uri", RedirectUri ?? "EMPTY REDIRECTURI")
+        FormUrlEncodedContent requestBody = new FormUrlEncodedContent([
+            new KeyValuePair<string, string>("grant_type", "authorization_code"),
+            new KeyValuePair<string, string>("code", code),
+            new KeyValuePair<string, string>("redirect_uri", RedirectUri ?? "EMPTY REDIRECTURI")
         ]);
 
-        var api = AppState.Instance.IsSandboxMode ? ".sandbox" : "";
-        var response = await client.PostAsync($"https://api{api}.ebay.com/identity/v1/oauth2/token", requestBody);
+        string api = AppState.Instance.IsSandboxMode ? ".sandbox" : "";
+        HttpResponseMessage response = await client.PostAsync($"https://api{api}.ebay.com/identity/v1/oauth2/token", requestBody);
 
         return response.Content.ReadAsStringAsync().Result;
     }
